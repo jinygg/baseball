ddf87d190f47ca0de2759eaa5fe1b84926b65bb3
yeongi.jin <jinygg78@gmail.com>
2025-07-23 13:01:52 +0900

    invalid number

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -3,6 +3,12 @@
 class Baseball {
 public:
 	void guess(const std::string& str) {
-		throw std::length_error("Must be 3 letter ");
+		if(str.length() != 3)
+			throw std::length_error("Must be 3 letter ");
+
+		for (auto ch : str) {
+			if (ch < '0' || ch > '9')
+				throw std::invalid_argument("Must be number");
+		}
 	}
 };
--- BaseBall/baseball_test.cpp
+++ BaseBall/baseball_test.cpp
@@ -5,3 +5,8 @@ TEST(BaseballGame, InputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(std::string("12")), std::length_error);
 }
+
+TEST(BaseballGame, InvalidCharacter) {
+	Baseball game;
+	EXPECT_THROW(game.guess(std::string("12s")), std::invalid_argument);
+}
