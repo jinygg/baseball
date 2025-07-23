6f5f1fa1e552fa10e5147a4542b02a3c1dd5b179
yeongi.jin <jinygg78@gmail.com>
2025-07-23 13:10:42 +0900

    change guess num

--- BaseBall/baseball.cpp
+++ BaseBall/baseball.cpp
@@ -2,11 +2,11 @@
 
 class Baseball {
 public:
-	void guess(const std::string& str) {
-		if(str.length() != 3)
+	void guess(const std::string& guessnum) {
+		if(guessnum.length() != 3)
 			throw std::length_error("Must be 3 letter ");
 
-		for (auto ch : str) {
+		for (auto ch : guessnum) {
 			if (ch < '0' || ch > '9')
 				throw std::invalid_argument("Must be number");
 		}
